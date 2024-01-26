995b1db9ed2ee865c155954522d3ff3946cda59f
AlexSerb <aserbin190@gmail.com>
2024-01-26 19:51:12 +0400

    Added tests for getProductsForOrder view

--- backend/api/tests/test_views.py
+++ backend/api/tests/test_views.py
@@ -3,6 +3,7 @@ from rest_framework.test import APIClient
 from django.urls import reverse
 from django.contrib.auth.models import Group
 from datetime import datetime
+from uuid import uuid4
 
 from accounts.models import User
 from api.models import *
@@ -32,9 +33,10 @@ class ViewsTest(TestCase):
         response = self.client.post('/accounts/token/', data={'email': self.email, 'password': self.password})
         self.token = response.data['access']
 
+
     def test_get_orders_correct(self):
         order = Order.objects.create(user=self.user, discount=0.05,
-            status=OrderStatus.objects.filter(name="At work").first()).save()
+            status=OrderStatus.objects.filter(name="At work").first())
         
         client = APIClient()
         client.credentials(HTTP_AUTHORIZATION='Bearer ' + self.token)
@@ -42,13 +44,50 @@ class ViewsTest(TestCase):
 
         self.assertEqual(response.status_code, 200)
         self.assertEqual(len(response.data), 1)
-        self.assertEqual(response.data[0]['status']['name'], 'At work')
-        self.assertEqual(response.data[0]['order_date'], datetime.now().strftime('%Y-%m-%d'))
-        self.assertEqual(response.data[0]['discount'], '0.05')
+        response_order = response.data[0]
+        self.assertEqual(response_order['status']['name'], 'At work')
+        self.assertEqual(response_order['order_date'], datetime.now().strftime('%Y-%m-%d'))
+        self.assertEqual(response_order['discount'], '0.05')
         
     def test_get_orders_incorrect_token(self):
         client = APIClient()
         client.credentials(HTTP_AUTHORIZATION='Bearer ' + self.token + '1')
         response = client.get(self.URL + '/orders/')
+
+        self.assertEqual(response.status_code, 401)
+
+    def test_get_products_for_order_correct(self):
+        order = Order.objects.create(user=self.user, discount=0.05,
+            status=OrderStatus.objects.filter(name='At work').first())
+
+        product1 = Product.objects.create(product_status=ProductStatus.objects.filter(name='A defect was found').first(),
+            product_type=ProductType.objects.filter(name='Product type 2').first(), order=order, amount=2)
+        product2 = Product.objects.create(product_status=ProductStatus.objects.filter(name='Ready').first(),
+            product_type=ProductType.objects.filter(name='Product type 1').first(), order=order, amount=1)
+
+        tooth1 = Tooth.objects.create(product=product1, tooth_number=13)
+        tooth2 = Tooth.objects.create(product=product1, tooth_number=12)
+        tooth3 = Tooth.objects.create(product=product2, tooth_number=25)
+        
+        client = APIClient()
+        client.credentials(HTTP_AUTHORIZATION='Bearer ' + self.token)
+        response = client.get(self.URL + '/products/' + str(order.id), follow=True)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.data), 2)
+        # Check first product
+        self.assertEqual(response.data[0]['product_status']['name'], 'A defect was found')
+        self.assertEqual(response.data[0]['product_type']['name'], 'Product type 2')
+        self.assertEqual(len(response.data[0]['teeth']), 2)
+        # Check second product 
+        self.assertEqual(response.data[1]['product_status']['name'], 'Ready')
+        self.assertEqual(response.data[1]['product_type']['name'], 'Product type 1')
+        self.assertEqual(len(response.data[1]['teeth']), 1)
+        self.assertEqual(response.data[1]['teeth'][0]['tooth_number'], 25)
+        
+    def test_get_products_for_order_incorrect_token(self):
+        client = APIClient()
+        client.credentials(HTTP_AUTHORIZATION='Bearer ' + self.token + '1')
+        response = client.get(self.URL + '/products/123', follow=True)
 
         self.assertEqual(response.status_code, 401)
