52f30ce479749f292a21f8498f1c89970957a3b1
AlexSerb <aserbin190@gmail.com>
2024-01-27 11:29:52 +0400

    Accelerated test execution

--- backend/manage.py
+++ backend/manage.py
@@ -8,6 +8,34 @@ def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
     try:
         from django.core.management import execute_from_command_line
+        from django.conf import settings
+
+        if 'test' in sys.argv:
+            import logging
+            logging.disable(logging.CRITICAL)
+            settings.DEBUG = False
+            settings.PASSWORD_HASHERS = [
+                'django.contrib.auth.hashers.MD5PasswordHasher',
+            ]
+
+        if 'test' in sys.argv and '--time' in sys.argv:
+            sys.argv.remove('--time')
+            from django import test
+            import time
+
+            def setUp(self):
+                self.startTime = time.time()
+
+            def tearDown(self):
+                total = time.time() - self.startTime
+                if total > 0.5:
+                    print("nt033[91m%.3fst%s033[0m" % ( \
+                        total, self._testMethodName))
+                
+            test.TestCase.setUp = setUp
+            test.TestCase.tearDown = tearDown
+
+
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
